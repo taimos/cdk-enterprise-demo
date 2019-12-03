e1b5873f588937728c082d50f0d28602d0617259
Andrew Hammond <andrew.hammond@helix.com>
2019-12-03 14:00:16 -0800

    allow any type of encryption for buckets

--- lib/bucket.ts
+++ lib/bucket.ts
@@ -7,10 +7,18 @@ import { Construct } from '@aws-cdk/core';
 export class AcmeBucket extends Bucket {
 
     constructor(scope: Construct, id: string, props?: BucketProps) {
+
+        // Do not allow un-encrypted buckets
+        if (props.encryption) {
+            if (props.encryption == BucketEncryption.UNENCRYPTED) {
+                throw new Error('You must encrypt your buckets.');
+            }
+        }
+
         super(scope, id, {
             ...props,
             blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-            encryption: BucketEncryption.KMS_MANAGED,
+            encryption: props.encryption ? props.encryption : BucketEncryption.KMS_MANAGED,
             publicReadAccess: false,
             accessControl: BucketAccessControl.PRIVATE,
         });
